134c94211e2a65df3ac02a6a0227bfd01b870177
ChanX96 <821602490@qq.com>
2019-10-06 22:59:20 +0800

    使用xml管理log4j

---
+++ src/main/java/Log4jTest.java
@@ -0,0 +1,10 @@
+import org.apache.log4j.Logger;
+
+public class Log4jTest {
+
+    private final static Logger logger = Logger.getLogger(Log4jTest.class);
+
+    public static void main(String[] args) {
+        logger.error("error");
+    }
+}
