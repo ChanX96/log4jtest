9c1fe5be28f3e8a0d170d9c71ed21708ad8c18a8
ChanX <821602490@qq.com>
2019-10-08 12:53:33 +0800

    增加了一个warning logger

--- src/main/java/Log4jTest.java
+++ src/main/java/Log4jTest.java
@@ -5,6 +5,7 @@ public class Log4jTest {
     private final static Logger logger = Logger.getLogger(Log4jTest.class);
 
     public static void main(String[] args) {
+        logger.warn("warn");
         logger.error("error");
     }
 }
